a7cbe1dce96599ae2bd8501c93aef7a2a3aed6a9
Mohamed <mohamed.atef7765@gmail.com>
2025-10-31 20:57:05 +0200

    Section 3: Printing

---
+++ Section03_Printing/03_Exercises/05_correct_the_following/main.cpp
@@ -0,0 +1,74 @@
+/*
+============================================================
+ Home Work Title   : correct the following
+ Module        : Section 03 – Printing
+ Author        : <Add author name>
+ Created On    : <Add creation date>
+ Draft Version : v0.1
+============================================================
+ Overview
+ --------
+
+
+ Key Notes
+ ---------
+
+
+ Testing Checklist
+ -----------------
+
+------------------------------------------------------------
+*/
+
+/*============================================================
+ File Inclusions
+============================================================*/
+#include<stream  /* Correction: #include <iostream> */
+
+/*============================================================
+ Used Namespaces
+============================================================*/
+using namespace STD /* using namespace std;*/
+/*============================================================
+ Type Definitions
+============================================================*/
+
+/*============================================================
+ Local & Global Macros
+============================================================*/
+
+/*============================================================
+ Function-like Macros
+============================================================*/
+
+/*============================================================
+ Static Function Declarations
+============================================================*/
+
+/*============================================================
+ Global Function Declarations
+============================================================*/
+
+/*============================================================
+ Static Function Definitions
+============================================================*/
+
+/*============================================================
+ Global Function Definitions
+============================================================*/
+
+
+cout <<"work_smart_not_hard\n"; /* You can't make line code without an outer function */
+
+int maIN() { /* The spelling of 'main' is wrong */
+
+    cout << "Children must be taught how to think, not what to think" /* Missing Semi Column*/
+    cout << "We worry about what a child will become "tomorrow", yet we forget that he is someone today\n"; /*Usage of "" without \*/
+    cout << "Children are not things to be molded"<", but are people to be unfolded\n"; /* single < is not an output operator */
+    cout << "Each day of our lives we make deposits in the memory banks of our children."<end;  /* single < is not an output operator */
+    cout  << ""It is easier to build strong children than to repair broken men""<<"\n"; /* Usage of double quotes twice */
+    cout >> "Children need models rather than critics\n"; /* Using insertion operator instead of output operator */
+    out<<"Children have never been very good at listening to their elders, but they have never failed to imitate them"; /* Using out instead of cout */
+    cout < "Children are our most valuable resource\n"; /* Single < is not an output operator */
+}
+
